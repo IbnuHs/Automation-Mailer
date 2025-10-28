05d364b8476e486ab6ec58ec64e5541395448abd
IbnuHs <ibnuhs30@gmail.com>
2025-10-28 23:44:01 +0900

    uhh

--- src/AuthConfig.js
+++ src/AuthConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "c630ee8c-07bb-4ca6-9755-3563c744057c",
     authority: "https://login.microsoftonline.com/common/",
-    redirectUri: "https://automation-mailer.vercel.app/broadcast",
+    redirectUri: "https://automation-mailer.vercel.app",
   },
   cache: {
     cahceLocation: "localStorage",
